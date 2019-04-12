6e02d171bae8023a7a479a99e22fdf8dfe7321e5
mstranger <mr.mstranger@gmail.com>
2019-04-12 19:05:18 +0300

    Interface http.Handler

---
+++ chapter7/http4.go
@@ -0,0 +1,39 @@
+// p. 231
+package main
+
+// TODO: p. 236, tasks 7.11 - 7.12
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type dollars float32
+type database map[string]dollars
+
+func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprintf(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) price(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price, ok := db[item]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound) // 404
+		fmt.Fprintf(w, "no such item: %q\n", item)
+		return
+	}
+	fmt.Fprintf(w, "%s\n", price)
+}
+
+func main() {
+	db := database{"shoes": 50, "socks": 5}
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/price", db.price)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
