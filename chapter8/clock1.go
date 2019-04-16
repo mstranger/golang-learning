2889ad7c0aaf47de5fba2ae5628c21b9aa85f90b
mstranger <mr.mstranger@gmail.com>
2019-04-16 19:09:25 +0300

    Chapter 8. Goroutines

---
+++ chapter8/clock1.go
@@ -0,0 +1,35 @@
+// p. 262
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // for example, client disconnect
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // lost connection
+			continue
+		}
+		handleConn(conn)
+	}
+}
